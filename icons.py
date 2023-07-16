36a756264134ff3faafc45ccd86bff658b95e575
LKAMinco <milanhrabovsky29@gmail.com>
2023-07-16 17:44:44 +0200

    Fix absolute paths

--- icons.py
+++ icons.py
@@ -1,5 +1,9 @@
 import bpy
 from bpy.utils import previews
+from pathlib import Path
+from os import path
+
+icons_dir = str(Path(__file__).parent / 'icons')
 
 icons = previews.new()
 
@@ -16,6 +20,6 @@ icons_files = ['IGNORE',
 for icon in icons_files:
     icons.load(
         name=icon,
-        path='icons/' + icon + '.png',
+        path=path.join(icons_dir, icon + '.png'),
         path_type='IMAGE'
     )
